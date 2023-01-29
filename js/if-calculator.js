ba654cc6a66a971b62c7e04c4e9d65a8c76c5829
Heinrich <verwey41@gmail.com>
2023-01-29 21:28:33 +0200

    If Calculator finished
    Has a problem that I can't seem to get rid off

---
+++ js/if-calculator.js
@@ -0,0 +1,29 @@
+function calculate() {
+    const inputOne = parseInt(document.getElementById("inputOne"));
+    const inputTwo = parseInt(document.getElementById("inputTwo"));
+    let op = document.getElementById("op");
+    
+    if (op === "+" || op === "-" || op === "*" || op === "/" ) {
+        if (op === "+") {
+             result = inputOne + inputTwo;
+            console.log("You calculated " + result);
+            document.getElementById("answer").innerHTML = "You calculated  " + inputOne + "+" + inputTwo + "=" + result ;
+        } else if (op === "-") {
+             result = inputOne - inputTwo;
+            console.log("You calculated " + result);
+            document.getElementById("answer").innerHTML = "You calculated  " + inputOne + "-" + inputTwo + "=" + result ;
+        } else if (op === "*") {
+             result = inputOne * inputTwo;
+            console.log("You calculated " + result);
+            document.getElementById("answer").innerHTML = "You calculated  " + inputOne + "*" + inputTwo + "=" + result ;
+        } else if (op === "/") {
+             result = inputOne / inputTwo;
+            console.log("You calculated " + result);
+            document.getElementById("answer").innerHTML = "You calculated  " + inputOne + "/" + inputTwo + "=" + result ;
+        }
+    } 
+    else {
+        console.log("Invalid operator")
+        alert("It looks like you have entered the wrong operator. Please enter either one of the following + or - or * or /.")
+    }
+}
---
+++ js/switch-calculator.js
@@ -0,0 +1,15 @@
+function calculate() {
+    const inputOne = parseInt(document.getElementById("inputOne"));
+    const inputTwo = parseInt(document.getElementById("inputTwo"));
+    const op = document.getElementById("op");
+
+    switch(op) {
+        case "+": 
+            
+        case "-": 
+
+        case "*": 
+
+        case "/": 
+    }
+}
